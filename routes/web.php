823beee0e69cf5015999df8bb3dc7214bab7c2d4
amit-bhardwaj19 <amit.bhardwaj87@yahoo.com>
2022-11-27 17:30:45 +0530

    contrain route parameter with RouteServiceProvider.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts/{id}', function ($id) {
+    return 'Blog Post '.$id;
+})->name('posts.show');
